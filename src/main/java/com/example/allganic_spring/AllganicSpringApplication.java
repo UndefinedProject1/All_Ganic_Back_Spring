f23d5c14afea844d2dd19c4c649db2a80ff831b8
kongjihi <kong3361@naver.com>
2021-10-18 13:02:21 +0900

    123

---
+++ src/main/java/com/example/allganic_spring/AllganicSpringApplication.java
@@ -0,0 +1,14 @@
+package com.example.allganic_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllganicSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllganicSpringApplication.class, args);
+		System.out.println("start");
+	}
+
+}
