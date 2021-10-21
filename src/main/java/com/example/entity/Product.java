af34b8eaf21b3120277cfe42aabe7e90406c2bec
[Insa14] <[kong3361@naver.com]>
2021-10-21 11:40:29 +0900

    entity 수정

--- src/main/java/com/example/entity/Product.java
+++ src/main/java/com/example/entity/Product.java
@@ -4,10 +4,13 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -24,9 +27,11 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 @Table(name = "PRODUCT")
+@SequenceGenerator(name = "SEQ_PRODUCT_NO", sequenceName = "SEQ_PRODUCT_NO", initialValue = 1, allocationSize = 1)
 public class Product {
     @Id
     @Column(name = "PRODCUTCODE")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_PRODUCT_NO")
     private long productcode = 0L;
 
     @Column(name = "PRODCUTNAME")
