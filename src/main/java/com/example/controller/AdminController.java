8c401f3be9958ffae54769709f6161e8e5fd4e27
Johwanyeong <85853166+Johwanyeong@users.noreply.github.com>
2021-10-21 11:36:18 +0900

    111
    1

--- src/main/java/com/example/controller/AdminController.java
+++ src/main/java/com/example/controller/AdminController.java
@@ -97,9 +97,12 @@ public class AdminController {
     }
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     // 제품 삭제
     // 127.0.0.1:8080/REST/api/admin/product_delete
 =======
+=======
+>>>>>>> Stashed changes
     //물품 삭제
     //127.0.0.1:8080/REST/api/admin/product_delete
 >>>>>>> Stashed changes
@@ -118,6 +121,31 @@ public class AdminController {
     }
 =======
 
+    //물품 수정
+    //127.0.0.1:8080/REST/api/admin/product_update
+    @RequestMapping(value = "/admin/product_update", method = {
+        RequestMethod.POST}, consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+        public Map<String, Object> productUpdate(@RequestBody Product product,
+            @RequestHeader("token") String token) { 
+            Map<String, Object> map = new HashMap<>();
+            try{
+                // 관리자 이메일 가져오기
+                String adminId = jwtUtil.extractUsername(token.substring(7));
+                Member member = mServiece.getMemberOne(adminId);
+                product.setMember(member);
+                
+                pService.deleteProduct(product.getProductcode());
+                map.put("result",1);
+            }
+            catch(Exception e){
+                map.put("result",e.hashCode());
+            }
+            return map;
+        }
+
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
     //물품 수정
     //127.0.0.1:8080/REST/api/admin/product_update
     @RequestMapping(value = "/admin/product_update", method = {
